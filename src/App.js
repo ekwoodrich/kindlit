c3d67a34620e2d7143e4b464f386376f5036607a
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-11-23 12:29:17 -0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>kindlit</h1>
+        <h5 id="mainSubheader">send to kindle</h5>
+       
+        <input type="text" placeholder="Enter kindle email address" id="mainEmailInput"></input>
+        <Button variant="primary">Save email</Button>
+        <hr/>
+
+        <input type="text" placeholder="Book URL or title" id="mainBookInput"></input>
+        <Button variant="primary">Send to Kindle</Button>
+      <Image id="mainDragImage" src={require('./assets/drop.png')} />
+      <span id="mainDragMessage">drag and drop MOBI file</span>
       </header>
     </div>
   );
