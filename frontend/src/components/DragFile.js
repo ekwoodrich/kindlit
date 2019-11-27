ac9fa6c14ab4a7d2fc2a62f4ebf13559a2bf9c98
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-11-26 17:35:39 -0800

    Added dropzone component.

--- frontend/src/components/DragFile.js
+++ frontend/src/components/DragFile.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image';
+import Dropzone from 'react-dropzone'
 
 class DragFile extends Component {
+  
   render() {
     return (
       <div>
+
+        <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+  {({getRootProps, getInputProps}) => (
+    <section>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
         <Image id="mainDragImage" src={require('../assets/drop.png')} />
-        <span id="mainDragMessage">drag and drop MOBI or PDF</span>
+        <p>drag and drop MOBI or PDF</p>
+      </div>
+    </section>
+  )}
+</Dropzone>
+
+        {/* <span id="mainDragMessage">drag and drop MOBI or PDF</span> */}
       </div>
     );
   }
